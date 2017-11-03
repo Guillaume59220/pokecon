3706934bf4fa29f83fc8a52d288e4803037988ee
Guillaume-Batailler <guillaumedu59220@hotmail.com>
2017-11-03 16:55:17 +0100

    modification à 16:55

--- pokécon.php
+++ pokécon.php
@@ -5,12 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pokécon</title>
+
+    <link rel="stylesheet" href="pokecon.css">
 </head>
 <body>
-   <h1>Pokécon</h1> 
-<?php
-    
+   <h1>Pokécon</h1>
+
+   <h2>Le pokédex de la mort qui tue</h2>
+
+   <div class="image">
+        <img src="pokedex.jpg" alt="pokedex">
+   </div>
+
+   <div class="php">
+    <?php
+    echo "<h1>LOL</h1>"
 ?>
+   </div>
 
 </body>
 </html>
